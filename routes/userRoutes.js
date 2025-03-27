e31b31c2daf84d73ab1bb93767108c9caeb7aa3a
HerbertGozalie <132330396+HerbertGozalie@users.noreply.github.com>
2025-03-27 10:24:13 +0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,6 +19,6 @@ route.patch('/verifyemail', verifyEmail)
 route.post('/login', validateUserLogin, loginUser)
 route.post('/forgotpassword', forgotPassword)
 route.patch('/resetpassword', validateUserResetPassword, resetPassword)
-route.delete('/:uuid', deleteUser)
-
+//route.delete('/:uuid', deleteUser)
+route.delete('/delete', deleteUser)
 module.exports = route
